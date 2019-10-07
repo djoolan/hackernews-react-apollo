3757ec06850b37d40d84e1e5b6a2b3aadc441458
Julien Londeix <jlondeix@gmail.com>
2019-10-07 14:58:12 +0200

    Adding authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
+import Login from './Login'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -8,22 +8,34 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
 })
 
 ReactDOM.render(
     <BrowserRouter>
-      <ApolloProvider client={client}>
+        <ApolloProvider client={client}>
         <App />
-      </ApolloProvider>
+        </ApolloProvider>
     </BrowserRouter>,
     document.getElementById('root')
-  )
+)
 serviceWorker.unregister()
